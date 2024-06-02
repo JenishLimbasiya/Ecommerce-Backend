13e90290b3dba30aa982294dc87a1825ade663f0
JenishLimbasiya <j.b.limbasiya99@gmail.com>
2024-06-02 19:20:09 +0530

    add: crud category

--- source/common/config/roles.ts
+++ source/common/config/roles.ts
@@ -2,7 +2,12 @@ import constant from "./constant";
 const roles = [constant.ROLES.ADMIN, constant.ROLES.USER];
 const roleRights = new Map();
 
-roleRights.set(roles[0], ["addbox"]);
+roleRights.set(roles[0], [
+  "addCategory",
+  "editCategory",
+  "categoryList",
+  "deleteCategory",
+]);
 
 roleRights.set(roles[1], ["changePassword"]);
 
